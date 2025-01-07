f73bd4185659874b9ae85ff8f7e3dc146747c353
julianbrom1 <bec21001@byui.edu>
2025-01-06 22:48:04 -0500

    Practicing

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!!! This's the Sandbox Project.");
+        Console.WriteLine("This is in C#");
+
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}");
+
     }
 }
