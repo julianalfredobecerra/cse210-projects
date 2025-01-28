3fd35c9d980c0aa0576e6a662c2a2692af283b4c
julianbrom1 <bec21001@byui.edu>
2025-01-28 01:01:52 -0500

    first

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -6,12 +6,21 @@ class Program
 {
         static void Main(string[] args)
         {
-        Fraction fraction1 = new Fraction();
-        Fraction fraction2 = new Fraction(6);
-        Fraction fraction3 = new Fraction(6, 7);
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
 
-        Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()} (Decimal: {fraction1.GetDecimalValue()})");
-        Console.WriteLine($"Fraction 2: {fraction2.GetFractionString()} (Decimal: {fraction2.GetDecimalValue()})");
-        Console.WriteLine($"Fraction 3: {fraction3.GetFractionString()} (Decimal: {fraction3.GetDecimalValue()})");
-        }
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+    }
+}
 }
