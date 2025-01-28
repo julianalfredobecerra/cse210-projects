1dda49d050752d3faf47a224211114a3c9433324
julianbrom1 <bec21001@byui.edu>
2025-01-28 13:29:06 -0500

    Explain the code.

--- week03/Fraction.cs
+++ week03/Fraction.cs
@@ -2,18 +2,25 @@ using System;
 
 public class Fraction
 {
+//Create a class to hold fraction.
+//The class should be in its own file.
+//The class should have two attributes for the top and bottom numbers.
+//Make sure the attributes are private.
     private int _top;
     private int _bottom;
+    //Constructor that has no parameters that initializes the number to 1/1.
     public Fraction()
     {
     _top = 1;
     _bottom = 1;
     }
+    //Constructor that has one parameter for the top and that initializes the denominator to 1. So that if you pass in the number 5, the fraction would be initialized to 5/1.
     public Fraction(int top)
     {
     _top = top;
     _bottom = 1;
     }
+    //Constructor that has two parameters, one for the top and one for the bottom.
     public Fraction(int top, int bottom)
     {
     if (bottom == 0)
@@ -23,6 +30,7 @@ public class Fraction
     _top = top;
     _bottom = bottom;
     }
+    //Create getters and setters for both the top and the bottom values.
     public int GetTop()
     {
     return _top;
@@ -43,10 +51,12 @@ public class Fraction
     }
     _bottom = bottom;
     }
+    //Create a method called GetFractionString that returns the fraction in the form 3/4.
     public string GetFractionString()
     {
     return $"{_top}/{_bottom}";
     }
+    //Create a method called GetDecimalValue that returns a double that is the result of dividing the top number by the bottom number, such as 0.75.
     public double GetDecimalValue()
     {
     return (double)_top / _bottom;
